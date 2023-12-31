a07dce20041d5786794a11ab44bb481b7296e7e6
youssef <83027476+youissef@users.noreply.github.com>
2023-12-31 17:27:05 +0100

    if else.c
    condition if else ...

---
+++ Apprenez à programmer en C/2/app.c
@@ -0,0 +1,72 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(int argc, char *argv[])
+// {
+//   int age = 20;
+
+//   if (age >= 18)
+//   {
+//     printf("Vous etes majeur !\n");
+//   }
+
+//   return 0;
+// }
+// //
+// if (/* Votre condition */)
+// {
+//   // Instructions à exécuter si la condition est vraie
+// }
+// //
+// if (age >= 18)
+// {
+//   printf("Vous etes majeur !");
+// }
+// if (age >= 18) // Si l'âge est supérieur ou égal à 18
+// {
+//   printf("Vous etes majeur !");
+// }
+// else // Sinon...
+// {
+//   printf("Ah c'est bete, vous etes mineur !");
+// }
+// //
+// if (age > 30 || argent > 100000)
+// {
+//   printf("Bienvenue chez PicsouBanque !");
+// }
+// else
+// {
+//   printf("Hors de ma vue, miserable !");
+// }
+// if (1)
+// {
+//   printf("C'est vrai");
+// }
+// else
+// {
+//   printf("C'est faux");
+// }
+// if (0)
+// {
+//   printf("C'est vrai");
+// }
+// else
+// {
+//   printf("C'est faux");
+// }
+// int age = 20;
+// int majeur = 0;
+
+// majeur = age >= 18;
+// printf("Majeur vaut : %d\n", majeur);
+// int majeur = 1;
+
+// if (majeur)
+// {
+//   printf("Vous etes majeur !");
+// }
+// else
+// {
+//   printf("Vous etes mineur");
+// }
